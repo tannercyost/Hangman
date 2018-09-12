0a7fba06315e035a4207735d25067cb4f3e9f7af
Tanner Yost <tyost@csumb.edu>
2018-09-11 19:11:55 -0700

    Added some methods and outlined a todo

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,8 +4,28 @@
  * Tanner Yost
  * 9/10/2018
  */
+import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args) {
+        System.out.println(getPhrase());
+    }
 
+    /**
+     * this method asks the user for a phrase to use for the game.
+     * @return
+     */
+    public static String getPhrase() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        System.out.println();
+        return sc.nextLine().toUpperCase().trim();
+    }
+
+    public static void makeGuess() {
+        //TODO: create method that proceeds with making a guess
+    }
+
+    public static void getHint() {
+        //TODO: create method that generates a hint for the user
     }
 }
