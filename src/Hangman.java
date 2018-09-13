60af54c57bd5163a6694be8c9ddf9375438c2162
Tanner Yost <tyost@csumb.edu>
2018-09-12 23:03:33 -0700

    Finished most of the logic of the program.

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,32 +4,76 @@
  * Tanner Yost
  * 9/10/2018
  */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-        String phrase = getPhrase();
+        //char[] phrase = getPhrase().toCharArray();
+        char[] phrase = "look a phrase".toCharArray();
+        boolean[] letterFound = new boolean[phrase.length];
+        boolean solved = false;
+        int guesses = 4;
+
+        displayWord(phrase, letterFound);
+
+        do {
+            try {
+                System.out.print("\nEnter either 1 for guessing or 2 for hint: ");
+                Scanner sc = new Scanner(System.in);
+
+                int choice = sc.nextInt();
+
+                if (choice == 1) {
+                    solved = makeGuess(phrase, letterFound, guesses);
+                } else if (choice == 2) {
+                    getHint();
+                } else {
+                    System.out.println("Incorrect input, only valid entries are 1 or 2.");
+                }
+            }
+            catch (InputMismatchException e) { System.out.println("Invalid input type."); }
+        } while(!solved);
+
     }
 
     /**
      * Asks the user for a phrase to use for the game.
      * @return
      */
-    public static String getPhrase() {
+    private static String getPhrase() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word: ");
         return sc.nextLine().toUpperCase().trim();
     }
 
-    public static void makeGuess() {
+    public static boolean makeGuess(char[] word, boolean[] display, int guesses) {
         //TODO: create method that proceeds with making a guess
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        if(choice == 1)
+            return true;
+        else if (choice == 2)
+            return false;
+        else
+            return false;
     }
 
     public static void getHint() {
         //TODO: create method that generates a hint for the user
     }
 
-    public static void displayWord() {
-        //TODO: create method that prints out the word as it currently stands
+    public static void displayWord(char[] word, boolean[] display) {
+        int index = 0;
+        System.out.print("So far, the word is: ");
+        for (char item : word) {
+            if (item==' ')
+                System.out.print("# ");
+            else if (display[index])
+                System.out.print(item+" ");
+            else
+                System.out.print("_ ");
+            index++;
+        }
     }
 }
